1028a492a2beb80178246c6e2f8c87c8769ac386
Dylan du Toit <dylan.du.toit@gmail.com>
2024-12-02 13:21:18 +0200

    Project init with React, MUI, Axios, etc

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Container } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <Container maxWidth="lg" className="MainContainer">
+        Hello World
+      </Container>
+    </div>
+  );
+}
+
+export default App;
